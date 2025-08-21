d55787b3db07c5d8bf0fd0b19202614ae04ef4db
RicardoGz <163072550+RicadoGz@users.noreply.github.com>
2025-08-20 21:08:31 -0400

    finish show the rest at home page

--- home/views.py
+++ home/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from .models import RestaurantProfile
 
-# Create your views here.
+@api_view(['GET'])
+def api_home(request, format=None):
+    """
+    The entry point for our API.
+    """
+    # Get the first profile object. If it doesn't exist, create it with the default name.
+    # This ensures the site works correctly on the first run.
+    #if not have it just creat one 
+    profile, created = RestaurantProfile.objects.get_or_create(pk=1)
+
+    return Response({
+        'restaurant_name': profile.name,
+       
+    })
